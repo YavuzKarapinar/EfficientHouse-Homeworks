17406397200765898dafbdfe3b7ec5560e9f86c5
YavuzKarapinar <57010842+YavuzKarapinar@users.noreply.github.com>
2022-12-25 14:47:29 +0300

    Update Q4.java

--- src/homework6/Q4.java
+++ src/homework6/Q4.java
@@ -1,22 +1,54 @@
 package homework6;
 
-public class Q4 { // hatalı
+public class Q4 { 
     public static void main(String[] args) {
-        int heigth = 5, width = 5, counter = 0;
-        for(int i = 0; i < 5; i++) {
+        display();
+    }
+
+    public static void display() {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        System.out.print("Input length and width: ");
+        int heigth = scanner.nextInt();
+        int width = scanner.nextInt();
+
+        reboundedStar(heigth, width);
+
+    }
+
+    public static void reboundedStar(int heigth, int width) {
+        boolean right = false;
+        int counter = 0;
+
+        for(int i = 0; i < heigth; i++) {
 
             System.out.print("|");
 
 
-            for(int j = 0; j < width; j++) {
-                if(counter == j) System.out.print("*");
-                else System.out.print(" ");
+            if(!right) {
+                makingStar(counter, width);
                 counter++;
             }
+            if(counter == width)
+                right = true;
 
-            counter = 0;
+            if(right) {
+                makingStar(counter, width);
+                counter--;
+            }
+
+            if(counter == 0)
+                right = false;
+
             System.out.print("|");
             System.out.println();
+        }
+    }
+
+    public static void makingStar(int counter, int width) {
+        for (int j = 0; j < width; j++) {
+            if (counter == j) System.out.print("*");
+            else System.out.print(" ");
         }
     }
 }
