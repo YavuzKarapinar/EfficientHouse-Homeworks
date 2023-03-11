08850abf962ccbb40e671c2ab163d66e666e2306
YavuzKarapinar <57010842+YavuzKarapinar@users.noreply.github.com>
2023-03-11 15:31:34 +0300

    homework 7

---
+++ src/homework7/Q2.java
@@ -0,0 +1,54 @@
+package homework7;
+
+public class Q2 {
+    public static void main(String[] args) {
+        run();
+    }
+
+    public static void run() {
+        java.util.Scanner kb = new java.util.Scanner(System.in);
+
+        SameDice d = new SameDice();
+
+        for (;;) {
+            System.out.print("How many times do you want to throw the dices ? ");
+            int n = Integer.parseInt(kb.nextLine());
+
+            if (n <= 0)
+                break;
+
+            d.throwDice(n);
+
+        }
+    }
+}
+
+class SameDice {
+    public double rate;
+
+    public int getSameCount(int n) {
+
+        java.util.Random r = new java.util.Random();
+        int count = 0;
+
+        for(int i = 0; i < n; i++) {
+            int d1 = r.nextInt(6) + 1;
+            int d2 = r.nextInt(6) + 1;
+
+            if(d1 == d2) {
+                System.out.println(d1 + " " + d2);
+                ++count;
+            }
+        }
+        System.out.println("Count: " + count);
+        return count;
+    }
+
+    public void throwDice(int n) {
+        rate = getSameCount(n) / (double)n;
+
+        System.out.println("Rate: " + rate);
+    }
+
+}
+
